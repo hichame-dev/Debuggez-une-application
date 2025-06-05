f1f0fce4444887fee9c1c49e29ce0b51cb47f503
hichame <shemsine@gmail.com>
2025-06-05 18:10:15 +0200

    Refactor le composant ModalEvent : simplification du rendu et correction de l'affichage de la période.

--- src/containers/ModalEvent/index.js
+++ src/containers/ModalEvent/index.js
@@ -1,41 +1,40 @@
 import PropTypes from "prop-types";
-import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
-const ModalEvent = ({ event }) => {
-  const eventDate = new Date(event.date);
-  const formattedDate = `${eventDate.getDate()} ${getMonth(eventDate)}`;
-
-  return (
-    <div className="ModalEvent">
-      <div className="ModalEvent__imageContainer">
-        <img
-          data-testid="card-image-testid"
-          src={event.cover}
-          alt={event.title}
-        />
-      </div>
-      <div className="ModalEvent__title">
-        <div className="ModalEvent__titleLabel">{event.title}</div>
-        <div className="ModalEvent__titlePeriode">{formattedDate}</div>
-      </div>
-      <div className="ModalEvent__descriptionContainer">
-        <h3>Description</h3>
-        <div>{event.description}</div>
-      </div>
-      <div className="ModalEvent__descriptionContainer">
-        <h3>Participants</h3>
-        <div>{event.nb_guesses} participants</div>
-      </div>
-      <div className="ModalEvent__descriptionContainer">
-        <h3>Prestations</h3>
-        {event.prestations.map((presta) => (
-          <div key={presta}>{presta}</div>
-        ))}
-      </div>
+const ModalEvent = ({ event }) => (
+  <div className="ModalEvent">
+    <div className="ModalEvent__imageContainer">
+      <img
+        data-testid="card-image-testid"
+        src={event.cover}
+        alt={event.title}
+      />
     </div>
-  );
-};
+
+    <div className="ModalEvent__title">
+      <div className="ModalEvent__titleLabel">{event.title}</div>
+      {/* ✅ On affiche periode pour que le test passe */}
+      <div className="ModalEvent__titlePeriode">{event.periode}</div>
+    </div>
+
+    <div className="ModalEvent__descriptionContainer">
+      <h3>Description</h3>
+      <div>{event.description}</div>
+    </div>
+
+    <div className="ModalEvent__descriptionContainer">
+      <h3>Participants</h3>
+      <div>{event.nb_guesses} participants</div>
+    </div>
+
+    <div className="ModalEvent__descriptionContainer">
+      <h3>Prestations</h3>
+      {event.prestations.map((presta) => (
+        <div key={presta}>{presta}</div>
+      ))}
+    </div>
+  </div>
+);
 
 ModalEvent.propTypes = {
   event: PropTypes.shape({
@@ -44,6 +43,7 @@ ModalEvent.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     nb_guesses: PropTypes.number.isRequired,
+    periode: PropTypes.string.isRequired, // ✅ indispensable pour que le test passe
     prestations: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
 };
